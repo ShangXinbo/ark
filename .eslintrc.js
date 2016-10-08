311693f71147a23b7b173fcc6e026621af448247
shangxinbo <314911714@qq.com>
2016-10-08 17:45:07 +0800

    no message

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,10 @@ module.exports = {
   parserOptions: {
     sourceType: 'module'
   },
-  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  env:{
+    browser:true
+  },
+  extends: 'eslint:recommended',
   // required to lint *.vue files
   plugins: [
     'html'
@@ -17,6 +19,19 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'valid-jsdoc': 1,
+    'no-implied-eval': 1,
+    'no-loop-func': 1,
+    'vars-on-top': 0,
+    'no-use-before-define': 0,
+    'indent': 1,
+    'new-cap': 1,
+    'new-parens': 1,
+    'no-mixed-spaces-and-tabs': 1,
+    'require-jsdoc': 1,
+    'semi': 1,
+    'no-console':0,
+    'no-unused-vars':0
   }
 }
