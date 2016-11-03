920f12a99814a33dd6b1d40db249a8b5e779ec65
shangxinbo <314911714@qq.com>
2016-11-03 19:19:23 +0800

    no message

--- src/components/analysis/sex.vue
+++ src/components/analysis/sex.vue
@@ -6,58 +6,64 @@
 </template>
 <script>
 
-import echarts from 'src/services/echarts.min';
+import echarts from 'echarts';
+import store from 'src/vuex/store';
 
-var myChart = echarts.init(document.getElementById('genders'));
-var key = ['男', '女'], value = [data['男'] ? data['男'] : 0, data['女'] ? data['女'] : 0];
-var option = {
-    legend: {
-        orient: 'horizontal',
-        top: 5,
-        icon: 'rect',
-        data: key,
-        textStyle: {
-            color: "#FFF",
-            fontSize: '12'
-        },
-        selectedMode:false
-    },
-    series: [
-        {
-            type: 'pie',
-            radius: '55%',
-            center: ['50%', '50%'],
-            label: {
-                normal: {
-                    position: "inside",
-                    formatter: function (data) {
-                        return data.percent + '%'
-                    }
-                }
-            },
-            data: [
-                {
-                    value: parseFloat(value[0]),
-                    name: key[0],
-                    itemStyle: {
-                        normal: {
-                            color: "#E94D6A"
-                        }
-                    }
+export default {
+    mounted:function(){
+        var myChart = echarts.init(document.getElementById('genders'));
+        var data = store.state.charts.sex;
+        var key = ['男', '女'], value = [data['男'] ? data['男'] : 0, data['女'] ? data['女'] : 0];
+        var option = {
+            legend: {
+                orient: 'horizontal',
+                top: 5,
+                icon: 'rect',
+                data: key,
+                textStyle: {
+                    color: "#FFF",
+                    fontSize: '12'
                 },
+                selectedMode:false
+            },
+            series: [
                 {
-                    value: parseFloat(value[1]),
-                    name: key[1],
-                    itemStyle: {
+                    type: 'pie',
+                    radius: '55%',
+                    center: ['50%', '50%'],
+                    label: {
                         normal: {
-                            color: "#09A88D"
+                            position: "inside",
+                            formatter: function (data) {
+                                return data.percent + '%'
+                            }
                         }
-                    }
+                    },
+                    data: [
+                        {
+                            value: parseFloat(value[0]),
+                            name: key[0],
+                            itemStyle: {
+                                normal: {
+                                    color: "#E94D6A"
+                                }
+                            }
+                        },
+                        {
+                            value: parseFloat(value[1]),
+                            name: key[1],
+                            itemStyle: {
+                                normal: {
+                                    color: "#09A88D"
+                                }
+                            }
+                        }
+                    ]
                 }
             ]
-        }
-    ]
+        };
+        myChart.setOption(option);
+    }
 };
-myChart.setOption(option);
 
 </script>
