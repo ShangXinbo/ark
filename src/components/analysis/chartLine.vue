bed7d4773e7ac20691722cf1a6138caf9416ca51
shangxinbo <314911714@qq.com>
2017-02-07 19:27:46 +0800

    lineChart 走通

--- src/components/analysis/chartLine.vue
+++ src/components/analysis/chartLine.vue
@@ -39,7 +39,11 @@
     export default {
         data: function() {
             return {
-                options: {},
+                options: {
+                    xAxis: [],
+                    yAxis: [],
+                    series: []
+                },
                 legend: []
             };
         },
